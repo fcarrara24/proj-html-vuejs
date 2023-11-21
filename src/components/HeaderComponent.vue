1be95baca59fe0526bcc9a4db918359ea1122b15
unknown <f.carrara24@studenti.unibg.it>
2023-11-21 16:16:46 +0100

    header-component base

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -4,8 +4,48 @@
             <div class="img-container">
                 <img class="header-height" src="../assets/img/dark-logo.png" alt="">
             </div>
-            <div>
+            <!-- navbar components  -->
+            <div class="d-flex flex-row justify-content-center gap-3 align-items-start position-relative ">
+                <div v-for="section in HeaderArray" class="dropdown h-100">
+                    <div class="dropbtn h-100 ">
+                        {{ section.name }}
+                    </div>
+                    <!-- sezione senza immagine -->
+                    <div class="dropdown-content bg-white position-absolute " v-if="!section.img">
+                        <div class="dropdown-section d-flex flex-column flex-wrap overflow-hidden ">
+                            <div class="reference-container d-flex flex-row justify-content-start align-items-start  pe-3 h-100"
+                                v-for="(reference, ind) in section.categories">
+                                <a :href="reference.link">
+                                    {{ reference.title }}
+                                </a>
+                                <div v-if="reference.relevance !== 'none'">
+                                    {{ reference.relevance }}
+                                </div>
+                            </div>
+                        </div>
 
+                    </div>
+                    <!-- sezione con immgine da completare -->
+                    <div v-else-if="section.img" class=" bg-white dropdown-with-image">
+                        <div class="dropdown-section d-flex flex-column flex-wrap overflow-hidden ">
+                            <div class="reference-container d-flex flex-row justify-content-start align-items-start  p-3 h-100"
+                                v-for="(reference, ind) in section.categories">
+                                <a :href="reference.link">
+                                    {{ reference.title }}
+                                </a>
+                                <div v-if="reference.relevance !== 'none'">
+                                    {{ reference.relevance }}
+                                </div>
+
+                            </div>
+                            <div class="image">
+                                <img width="200" height="100" :src="section.img" alt="">
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
 
             <div class="header-Socials d-flex flex-row justify-content-end  gap-3 align-items-center">
@@ -19,6 +59,7 @@
 </template>
 
 <script>
+import { navbarCoponents } from '@/data/navbarComponents.js'
 export default {
     name: 'HeaderComponent',
     data() {
@@ -44,18 +85,70 @@ export default {
                     link: '#',
                     icon: 'fa-brands fa-linkedin'
                 },
-            ]
+
+            ],
+            HeaderArray: navbarCoponents,
         }
     }
 }
 </script>
 
+
 <style lang="scss" scoped>
+@use '../assets/style/main' as *;
+@use '../assets/style/partials/variables' as *;
+
 .social-icons {
     font-size: 1.5em;
 }
 
 .header-height {
     max-height: 40px;
+}
+
+.dropdown {
+    position: relative;
+    display: inline-block;
+
+    &:hover>.dropdown-content {
+        display: block;
+        padding: 20px;
+        padding-left: 0;
+        padding-bottom: 20px;
+        min-width: 15vw;
+    }
+
+    &:hover>.dropdown-with-image {
+        display: block;
+    }
+}
+
+.dropdown-content {
+    display: none;
+    min-width: 15vw;
+
+}
+
+.dropdown-section {
+    max-height: 300px;
+    border-bottom: 3px solid $backgroundBtnOrange;
+}
+
+.dropdown-with-image {
+    display: none;
+    position: absolute;
+    min-width: 60vw;
+    transform: translate(-50%, 0);
+}
+
+.dropbtn:hover {
+
+    color: #EF6F31;
+
+}
+
+a {
+    text-decoration: none;
+    color: black;
 }
 </style>
---
+++ src/data/navbarComponents.js
@@ -0,0 +1,295 @@
+
+
+export const navbarCoponents = [
+    //home
+    {
+        name: 'Home',
+        img: './homepages-mega-menu-image-alt.jpg',
+        categories: [
+            {
+                title: 'MaxCoach Education',
+                relevance: 'HOT',
+                link: '#'
+            },
+            {
+                title: 'Course Portal',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Distant Learning',
+                relevance: 'HOT',
+                link: '#'
+            },
+            {
+                title: 'Multimedial Pedagogy',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Modern Schooling',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Remote Training',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Health Catching',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Course Portal',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Course Portal',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Mid Point Before',
+                relevance: 'none',
+                link: '#'
+            },
+            //midline
+            {
+                title: 'Mid Point After',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Course Portal',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Course Portal',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Course Portal',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Course Portal',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Course Portal',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Course Portal',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Sales Coaching',
+                relevance: 'NEW',
+                link: '#'
+            },
+            {
+                title: 'mental Theraopy',
+                relevance: 'NEW',
+                link: '#'
+            },
+        ]
+    },
+    //pages
+    {
+
+        name: 'Pages',
+        img: null,
+        categories: [
+            {
+                title: 'Start Here',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Success Story',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'About me',
+                relevance: 'HOT',
+                link: '#'
+            },
+            {
+                title: 'Start Here',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Success Story',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'About me',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Start Here',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Success Story',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'About me',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Start Here',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Success Story',
+                relevance: 'none',
+                link: '#'
+            },
+
+        ]
+    },
+    //courses
+    {
+        name: 'Courses',
+        img: null,
+        categories: [
+            {
+                title: 'Courses Grid 01',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Courses Grid 02',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Courses Grid 03',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Courses Grid 01',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Courses Grid 02',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Courses Grid 03',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Courses Grid 01',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Courses Grid 02',
+                relevance: 'none',
+                link: '#'
+            },
+
+        ]
+    },
+    //features
+    {
+        name: 'Features',
+        img: null,
+        categories: [
+            {
+                title: 'Events',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Zoom Meetings',
+                relevance: 'none',
+                link: '#'
+            },
+        ]
+    },
+    //blog
+    {
+        name: 'Blog',
+        img: null,
+        categories: [
+            {
+                title: 'Blog Grid',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Blog Masonry',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Blog Classic',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Blog List',
+                relevance: 'none',
+                link: '#'
+            },
+        ]
+    },
+    //shop
+    {
+        name: 'Shop',
+        img: null,
+        categories: [
+            {
+                title: 'Shop Left Sidebar',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Shop Right Sidebar',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Cart',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Wishlist',
+                relevance: 'none',
+                link: '#'
+            },
+            {
+                title: 'Single Product',
+                relevance: 'none',
+                link: '#'
+            },
+        ]
+    },
+]
