7850d28f01cb94872f512d7216f60313124d58aa
unknown <f.carrara24@studenti.unibg.it>
2023-11-22 12:44:04 +0100

    created footer array

---
+++ src/data/footbarComponent.js
@@ -0,0 +1,114 @@
+
+export const footbarCoponent = [
+    {
+        section: 'Address',
+        tag: '',
+        isLarge: true,
+        links: [
+            {
+                name: '382 NE 191st St # 87394 Miami, FL 33179-3899',
+                url: '#'
+            },
+            {
+                name: '+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)',
+                url: '#'
+            },
+            {
+                name: 'support@maxcoach.com',
+                url: '#'
+            },
+        ],
+        images: [],
+        socials: [
+            {
+                name: 'facebook',
+                class: 'fa-brands fa-square-facebook'
+            },
+            {
+                name: 'twitter',
+                class: 'fa-brands fa-twitter'
+            },
+            {
+                name: 'instagram',
+                class: 'fa-brands fa-instagram'
+            },
+            {
+                name: 'linedin',
+                class: 'fa-brands fa-linkedin'
+            }
+        ]
+    },
+    {
+        section: 'Explore',
+        tag: '',
+        isLarge: false,
+        links: [
+            {
+                name: 'Start here',
+                url: '#'
+            },
+            {
+                name: 'Success story',
+                url: '#'
+            },
+            {
+                name: 'Blog',
+                url: '#'
+            },
+            {
+                name: 'Courses',
+                url: '#'
+            },
+            {
+                name: 'Contact us',
+                url: '#'
+            }
+        ],
+        images: [],
+        socials: []
+    },
+    {
+        section: 'Information',
+        tag: '',
+        isLarge: false,
+        links: [
+            {
+                name: 'Membership',
+                url: '#'
+            },
+            {
+                name: 'Purchase guide',
+                url: '#'
+            },
+            {
+                name: 'Privacy policy',
+                url: '#'
+            },
+            {
+                name: 'Terms of services',
+                url: '#'
+            },
+
+        ],
+        images: [],
+        socials: []
+    },
+    {
+        section: 'Instagram',
+        tag: '@maxcoach',
+        isLarge: false,
+        links: [],
+        images: [
+            {
+                urlImg: '/img/artist_event_1.jpg'
+            },
+            {
+                urlImg: '/img/artist_event_2.jpg'
+            },
+            {
+                urlImg: '/img/artist_event_3.jpg'
+            },
+        ],
+        socials: []
+    },
+] 
